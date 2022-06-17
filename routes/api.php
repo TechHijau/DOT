8ac2d6e685b4a5b50a9edeed46f265afc12c1400
Rijuna <rickyjuliannasution@gmail.com>
2022-06-17 13:46:23 +0700

    penambahan authentication sanctum

--- routes/api.php
+++ routes/api.php
@@ -5,17 +5,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RajaOngkirApi;
 use App\Http\Controllers\ProvincesApi;
 use App\Http\Controllers\CitiesApi;
+use App\Http\Controllers\UserAuth;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::post('/register', [UserAuth::class, 'register']);
+Route::post('/login', [UserAuth::class, 'login']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
@@ -28,10 +21,14 @@ Route::controller(RajaOngkirApi::class)->group(function () {
 
 Route::controller(ProvincesApi::class)->group(function () {
     Route::get('/search/provinces', 'show');
-    Route::get('/swap/provinces', 'swap');
 });
 
 Route::controller(CitiesApi::class)->group(function () {
     Route::get('/search/cities', 'show');
-    Route::get('/swap/cities', 'swap');
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [UserAuth::class, 'logout']);
+    Route::get('/swap/provinces', [ProvincesApi::class, 'swap']);
+    Route::get('/swap/cities', [CitiesApi::class, 'swap']);
 });
